8a78d458a54a536bc7391bc9415dc65203d9c07d
Artur <buzov.artur@gmail.com>
2025-04-04 08:49:39 +0200

    add validation into CacheExpirationProperties

--- src/main/java/com/javaquasar/cache/conf/CacheExpirationProperties.java
+++ src/main/java/com/javaquasar/cache/conf/CacheExpirationProperties.java
@@ -1,5 +1,6 @@
 package com.javaquasar.cache.conf;
 
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,7 @@ import java.time.temporal.ChronoUnit;
 @ConfigurationProperties(prefix = "app.cache.expiration")
 public class CacheExpirationProperties {
 
+    @Min(1)
     private Integer value = 1;
     private ChronoUnit unit = ChronoUnit.MINUTES;
 
